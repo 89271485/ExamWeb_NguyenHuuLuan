052c2c2f2d5012bc52af3beb1fc17ba20f19534a
ADMIN <ADMIN@PC330>
2025-06-11 09:35:56 +0700

    Add project files.

---
+++ ExamWeb_NguyenHuuLuan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamWeb_NguyenHuuLuan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ExamWeb_NguyenHuuLuan/Models/Phim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamWeb_NguyenHuuLuan.Models
+{
+    public class Phim
+    {
+        public int Id { get; set; }
+        [StringLength(200)]
+        public string TuaDe { get; set; }
+        public string DienVien { get; set; }
+        public bool TrongNuoc { get; set; }
+        public double GiaVe { get; set; }     
+        public int ThoiLuong { get; set; }
+    }
+}
